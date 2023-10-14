bf3ddd3900449743f4e1f997da678d308f20c686
Daniel Estupiñán <danielestupinan0802@gmail.com>
2023-10-13 21:45:02 -0500

    Finish style toolbar

--- src/sections/product/product-details-toolbar.js
+++ src/sections/product/product-details-toolbar.js
@@ -11,6 +11,7 @@ import Stack from '@mui/material/Stack';
 import Iconify from 'src/components/iconify';
 import { RouterLink } from 'src/routes/components';
 import CustomPopover, { usePopover } from 'src/components/custom-popover';
+import { Divider } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
@@ -21,6 +22,7 @@ export default function ProductDetailsToolbar({
   liveLink,
   publishOptions,
   onChangePublish,
+  stateProduct,
   sx,
   ...other
 }) {
@@ -33,16 +35,18 @@ export default function ProductDetailsToolbar({
         direction="row"
         sx={{
           mb: { xs: 3, md: 5 },
-          ...sx,
+          ...sx
         }}
         {...other}
       >
         <Button
           component={RouterLink}
+          color="error"
+          variant="outlined"
           href={backLink}
           startIcon={<Iconify icon="eva:arrow-ios-back-fill" width={16} />}
         >
-          Back
+          Volver
         </Button>
 
         <Box sx={{ flexGrow: 1 }} />
@@ -55,31 +59,45 @@ export default function ProductDetailsToolbar({
           </Tooltip>
         )}
 
-        <Tooltip title="Edit">
-          <IconButton component={RouterLink} href={editLink}>
-            <Iconify icon="solar:pen-bold" />
-          </IconButton>
-        </Tooltip>
-
-        <LoadingButton
-          color="inherit"
-          variant="contained"
-          loading={!publish}
-          loadingIndicator="Loading…"
-          endIcon={<Iconify icon="eva:arrow-ios-downward-fill" />}
-          onClick={popover.onOpen}
-          sx={{ textTransform: 'capitalize' }}
+        <Button
+          variant="outlined"
+          color="primary"
+          component={RouterLink}
+          href={editLink}
+          startIcon={<Iconify icon="solar:pen-bold" width={18} />}
         >
-          {publish}
-        </LoadingButton>
+          Editar
+        </Button>
+        <Tooltip title="Crea una factura de comprar para agregar existencias a este producto">
+          <Button
+            variant="outlined"
+            color="primary"
+            component={RouterLink}
+            href={liveLink}
+            startIcon={<Iconify icon="gala:add" width={20} />}
+          >
+            Abastecer este producto
+          </Button>
+        </Tooltip>
+        <Button
+          variant="outlined"
+          color="primary"
+          component={RouterLink}
+          href={liveLink}
+          startIcon={<Iconify icon="gala:add" width={20} />}
+        >
+          Facturar este producto
+        </Button>
+        <Divider orientation="vertical" flexItem />
+
+        {stateProduct && (
+          <Button variant="outlined" color={stateProduct ? 'error' : 'primary'} component={RouterLink} href={liveLink}>
+            {stateProduct ? 'Desactivar' : 'Activar'}
+          </Button>
+        )}
       </Stack>
 
-      <CustomPopover
-        open={popover.open}
-        onClose={popover.onClose}
-        arrow="top-right"
-        sx={{ width: 140 }}
-      >
+      <CustomPopover open={popover.open} onClose={popover.onClose} arrow="top-right" sx={{ width: 140 }}>
         {publishOptions.map((option) => (
           <MenuItem
             key={option.value}
@@ -106,5 +124,5 @@ ProductDetailsToolbar.propTypes = {
   onChangePublish: PropTypes.func,
   publish: PropTypes.string,
   publishOptions: PropTypes.array,
-  sx: PropTypes.object,
+  sx: PropTypes.object
 };
