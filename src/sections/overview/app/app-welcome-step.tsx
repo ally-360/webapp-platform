db910be3a51b3018717a7bbd725ae5d7ddcf0c63
Daniel E <danielestupinan0802@gmail.com>
2025-12-29 18:45:03 -0500

    test

--- src/sections/overview/app/app-welcome-step.tsx
+++ src/sections/overview/app/app-welcome-step.tsx
@@ -31,7 +31,7 @@ const steps = [
     description:
       'Puedes crear tu primer producto haciendo click en el botón "Nuevo producto" o en el menú lateral. Puedes crear categorías, marcas y asociarlas a tu producto.',
     icon: 'solar:box-bold-duotone',
-    action: 'Crear producto',
+    action: 'Crear productos',
     img: '/assets/icons/faqs/ic_package.svg',
     url: paths.dashboard.inventory.newProduct
   },
